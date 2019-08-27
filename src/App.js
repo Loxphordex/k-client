8f795b6fffdf1e1a39671278f76e1b75da38d530
Silas Hallahan <silasishallahan@gmail.com>
2019-08-27 16:22:00 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 
+import Header from './components/Header/Header'
+
 function App() {
   return (
     <div className="App">
-      
+      <Route path='/' render={() => <Header />} />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default class HeaderRoute extends React.Component {
+export default class Header extends React.Component {
   render() {
     return(
-      <header>
-        <nav>
+      <header role='heading'>
+        <nav role='navigation'>
           <h2>Main Header</h2>
         </nav>
       </header>
