5bcdb6dd4dfd70af600c314b1aa6d7965dc582ac
Silas Hallahan <silasishallahan@gmail.com>
2019-08-27 15:24:57 -0700

    dev init

---
+++ src/routes/HeaderRoute/HeaderRoute.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class HeaderRoute extends React.Component {
+  render() {
+    return(
+      <header>
+        <nav>
+          <h2>Main Header</h2>
+        </nav>
+      </header>
+    )
+  }
+}
