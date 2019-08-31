2cf01ca17b1bea5247c1d085ef7f7764f5f3d52f
Silas Hallahan <silasishallahan@gmail.com>
2019-08-31 14:14:00 -0700

    login success

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import NewImageRoute from './routes/NewImageRoute/NewImageRoute'
 import AuthRoute from './routes/AuthRoute/AuthRoute'
+import LoginRoute from './routes/LoginRoute/LoginRoute'
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Route path='/' render={() => <Header />} />
         <Route path='/new' render={() => <NewImageRoute />} />
         <Route path='/auth' render={({ history }) => <AuthRoute history={history} />} />
+        <Route path='/login' render={({ history }) => <LoginRoute history={history} />} />
       </div>
     );
   }
