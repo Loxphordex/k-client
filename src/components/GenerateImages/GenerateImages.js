31cc5c3d2096800bb2beb1ed11138c29fc67ddb6
Silas Hallahan <silasishallahan@gmail.com>
2019-09-04 11:00:36 -0700

    login styles

--- src/components/GenerateImages/GenerateImages.js
+++ src/components/GenerateImages/GenerateImages.js
@@ -6,12 +6,14 @@ import ImageEdit from '../../components/ImageEdit/ImageEdit'
 export default class GenerateImages extends React.Component {
   render() {
     const { images } = this.props
+    const reverseImages = images.reverse()
+    console.log(images[0])
     const token = TokenServices.getJwt()
     return(
       <section className='generated-images'>
-        {images && images.map(image => {
+        {images && reverseImages.map(image => {
           return(
-            <div className='img-container'>
+            <div className='img-container' key={image.url}>
               { token && <ImageEdit /> }
               <GenerateImageElement pic={image} />
             </div>
