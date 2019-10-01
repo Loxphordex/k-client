e9caa5416349440b0ffee29e18cd6d2298c3f092
Silas Hallahan <silasishallahan@gmail.com>
2019-10-01 11:59:02 -0700

    contact basics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Gallery from './routes/Gallery/Gallery'
 import NewImageRoute from './routes/NewImageRoute/NewImageRoute'
 import AuthRoute from './routes/AuthRoute/AuthRoute'
 import LoginRoute from './routes/LoginRoute/LoginRoute'
+import Contact from './routes/Contact/Contact'
 
 class App extends React.Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class App extends React.Component {
         <Route path='/new' render={() => <NewImageRoute />} />
         <Route path='/auth' render={({ history }) => <AuthRoute history={history} />} />
         <Route path='/login' render={({ history }) => <LoginRoute history={history} />} />
+        <Route path='/contact' render={() => <Contact />} />
       </div>
     );
   }
---
+++ src/routes/Contact/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Contact.css'
+
+export default class Contact extends React.Component {
+  render() {
+    return(
+      <section className='contact-container'>
+        <h2 className='contact-header'>Contact</h2>
+        <hr className='contact-hr' />
+        <h3>Business Inquiry</h3>
+        <a href='mailto: pearegrineofficial@outlook.com'>pearegrineofficial@outlook.com</a>
+        <a href='mailto: keeganholcombharris@outlook.com'>keeganholcombharris@outlook.com</a>
+        <hr className='contact-hr' />
+        <h3>Social Media</h3>
+        <a target='_blank' href='www.instagram.com/pearegrineofficial'>Instagram</a>
+      </section>
+    )
+  }
+}
