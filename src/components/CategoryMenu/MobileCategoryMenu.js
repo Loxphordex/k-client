aae367b596b1ba2338a0f65280b21e05e1d33712
Silas <silasishallahan@gmail.com>
2021-02-03 19:43:16 -0800

    mobile menu details

---
+++ src/components/CategoryMenu/MobileCategoryMenu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './MobileCategoryMenu.css'
+
+export default function MobileCategoryMenu() {
+  return (
+    <div className='mobile-category-menu-container fade-in'>
+      <h3 className='menu-header categories-menu-header'>Categories</h3>
+      <ul className='mobile-nav-links mobile-category-menu-list'>
+        <li><Link to='/gallery/all'>All Clothing</Link></li>
+        <li><Link to='/gallery/shirts'>Shirts</Link></li>
+        <li><Link to='/gallery/sweatshirts'>Sweatshirts</Link></li>
+        <li><Link to='/gallery/jeans'>Jeans</Link></li>
+        <li><Link to='/gallery/hats'>Hats</Link></li>
+      </ul>
+    </div>
+  )
+}
--- src/components/Popout/Popout.js
+++ src/components/Popout/Popout.js
@@ -1,29 +1,44 @@
-import React from 'react'
-import textLogo from '../../images/PearegrineTextBlack.png'
+import React, { useEffect, useState } from 'react'
+import MobileCategoryMenu from '../CategoryMenu/MobileCategoryMenu'
 import { Link } from 'react-router-dom'
 
 export default function Popout({ popoutVisible, enableMenu }) {
+  const [detailsEnabled, setDetailsEnabled] = useState(false)
+
+  useEffect(() => {
+    return () => {
+      setDetailsEnabled(false)
+    }
+  }, [popoutVisible])
+
+  function toggleDetails(event) {
+    event.preventDefault()
+    event.stopPropagation()
+
+    setDetailsEnabled(!detailsEnabled)
+  }
+
   return (
-    <div className={`popout-container ${popoutVisible ? 'popout-visible' : 'popout-hidden'}`}>
+    <div className={`popout-container ${popoutVisible ? 'popout-visible' : 'popout-hidden'}`} onClick={() => enableMenu(false)}>
       <nav role='navigation' className='popout-nav'>
         <h3 className='menu-header'>Menu</h3>
         <ul className='mobile-nav-links'>
-          <li onClick={() => enableMenu(false)}>
+          <li>
             <Link to='/gallery/sale'>Sale</Link>
           </li>
-          <li onClick={() => enableMenu(false)}>
+          <li>
             <Link to='/gallery/arrivals'>New Arrivals</Link>
           </li>
-          <li>
+          <li onClick={(event) => toggleDetails(event)}>
             <Link to='/gallery/all'>Clothing</Link>
           </li>
-          <li onClick={() => enableMenu(false)}>
+          <li>
             <Link to='/discover'>Discover</Link>
           </li>
         </ul>
       </nav>
-      <div className='popout-details'>
-        
+      <div className={`popout-details fade-in ${detailsEnabled ? 'details-enabled-color' : String()}`}>
+        {detailsEnabled && <MobileCategoryMenu />}
       </div>
     </div>
   )
